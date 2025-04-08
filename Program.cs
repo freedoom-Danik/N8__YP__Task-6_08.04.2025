e83f665fbd92fa4ab6a566a8c6cb4a01068dfd9c
Aunt D <dzavizistur@gmail.com>
2025-04-08 12:57:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace N8__YP__Task_6_08._04._2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
