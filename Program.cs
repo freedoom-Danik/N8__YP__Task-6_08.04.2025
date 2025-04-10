334058557f492ada4dca51395d8ad8993a73481b
Aunt D <dzavizistur@gmail.com>
2025-04-10 10:39:50 +0300

    ful

--- Program.cs
+++ Program.cs
@@ -2,9 +2,36 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введите количество вершин многоугольника (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] x = new double[n];
+            double[] y = new double[n];
+
+            Console.WriteLine("Введите координаты вершин:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Вершина {i + 1}: ");
+                string[] coords = Console.ReadLine().Split(' ');
+                x[i] = double.Parse(coords[0]);
+                y[i] = double.Parse(coords[1]);
+            }
+
+            double area = PolygonArea(x, y, n);
+            Console.WriteLine($"Площадь многоугольника: {area:F2}");
+        }
+
+        static double PolygonArea(double[] x, double[] y, int n)
+        {
+            double area = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int j = (i + 1) % n; // Следующая вершина (циклически)
+                area += x[i] * y[j] - y[i] * x[j];
+            }
+            return Math.Abs(area) / 2.0;
         }
     }
 }
